ca8eede32d0695ee7af9f7ce915db835db9ee4f1
Lubosz Sarnecki <lubosz@gmail.com>
2015-10-01 17:10:26 +0200

    renderer: make image loading shorter

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -89,110 +89,47 @@ GLuint Renderer::initTexture(char const* Filename) {
     glBindTexture(Target, TextureName);
     glTexParameteri(Target, GL_TEXTURE_BASE_LEVEL, 0);
     glTexParameteri(Target,
-        GL_TEXTURE_MAX_LEVEL, static_cast<GLint>(Texture.levels() - 1));
-    /*
-    glTexParameteri(Target, GL_TEXTURE_SWIZZLE_R, Format.Swizzle[0]);
-    glTexParameteri(Target, GL_TEXTURE_SWIZZLE_G, Format.Swizzle[1]);
-    glTexParameteri(Target, GL_TEXTURE_SWIZZLE_B, Format.Swizzle[2]);
-    glTexParameteri(Target, GL_TEXTURE_SWIZZLE_A, Format.Swizzle[3]);
-    */
+                    GL_TEXTURE_MAX_LEVEL, static_cast<GLint>(Texture.levels() - 1));
+
     glm::tvec3<GLsizei> const Dimensions(Texture.dimensions());
     GLsizei const FaceTotal =
             static_cast<GLsizei>(Texture.layers() * Texture.faces());
 
-    switch (Texture.target()) {
-        case gli::TARGET_1D:
-            glTexStorage1D(
-                Target,
-                static_cast<GLint>(Texture.levels()),
-                Format.Internal, Dimensions.x);
-            break;
-        case gli::TARGET_1D_ARRAY:
-        case gli::TARGET_2D:
-        case gli::TARGET_CUBE:
-            glTexStorage2D(
+    glTexStorage2D(
                 Target, static_cast<GLint>(Texture.levels()), Format.Internal,
                 Dimensions.x,
                 Texture.target() == gli::TARGET_2D ? Dimensions.y : FaceTotal);
-            break;
-        case gli::TARGET_2D_ARRAY:
-        case gli::TARGET_3D:
-        case gli::TARGET_CUBE_ARRAY:
-            glTexStorage3D(
-                Target, static_cast<GLint>(Texture.levels()), Format.Internal,
-                Dimensions.x, Dimensions.y,
-                Texture.target() == gli::TARGET_3D ? Dimensions.z : FaceTotal);
-            break;
-        default:
-            assert(0);
-            break;
-    }
 
-    for (std::size_t Layer = 0; Layer < Texture.layers(); ++Layer)
-    for (std::size_t Face = 0; Face < Texture.faces(); ++Face)
-    for (std::size_t Level = 0; Level < Texture.levels(); ++Level) {
-        GLsizei const LayerGL = static_cast<GLsizei>(Layer);
-        glm::tvec3<GLsizei> Dimensions(Texture.dimensions(Level));
-        Target = gli::is_target_cube(Texture.target())
-            ? static_cast<GLenum>(GL_TEXTURE_CUBE_MAP_POSITIVE_X + Face)
-            : Target;
+    for (std::size_t Layer = 0;
+         Layer < Texture.layers(); ++Layer)
+        for (std::size_t Face = 0;
+             Face < Texture.faces(); ++Face)
+            for (std::size_t Level = 0;
+                 Level < Texture.levels(); ++Level) {
+                glm::tvec3<GLsizei> Dimensions(Texture.dimensions(Level));
 
-        switch (Texture.target()) {
-        case gli::TARGET_1D:
-            if (gli::is_compressed(Texture.format()))
-                glCompressedTexSubImage1D(
-                    Target, static_cast<GLint>(Level), 0, Dimensions.x,
-                    Format.Internal, static_cast<GLsizei>(Texture.size(Level)),
-                    Texture.data(Layer, Face, Level));
-            else
-                glTexSubImage1D(
-                    Target, static_cast<GLint>(Level), 0, Dimensions.x,
-                    Format.External, Format.Type,
-                    Texture.data(Layer, Face, Level));
-            break;
-        case gli::TARGET_1D_ARRAY:
-        case gli::TARGET_2D:
-        case gli::TARGET_CUBE:
-            if (gli::is_compressed(Texture.format()))
-                glCompressedTexSubImage2D(
-                    Target, static_cast<GLint>(Level),
-                    0, 0,
-                    Dimensions.x,
-                    Dimensions.y,
-                    Format.Internal, static_cast<GLsizei>(Texture.size(Level)),
-                    Texture.data(Layer, Face, Level));
-            else
-                glTexSubImage2D(
-                    Target, static_cast<GLint>(Level),
-                    0, 0,
-                    Dimensions.x,
-                    Dimensions.y,
-                    Format.External, Format.Type,
-                    Texture.data(Layer, Face, Level));
-            break;
-        case gli::TARGET_2D_ARRAY:
-        case gli::TARGET_3D:
-        case gli::TARGET_CUBE_ARRAY:
-            if (gli::is_compressed(Texture.format()))
-                glCompressedTexSubImage3D(
-                    Target, static_cast<GLint>(Level),
-                    0, 0, 0,
-                    Dimensions.x, Dimensions.y,
-                    Texture.target() == LayerGL,
-                    Format.Internal, static_cast<GLsizei>(Texture.size(Level)),
-                    Texture.data(Layer, Face, Level));
-            else
-                glTexSubImage3D(
-                    Target, static_cast<GLint>(Level),
-                    0, 0, 0,
-                    Dimensions.x, Dimensions.y,
-                    Texture.target() == LayerGL,
-                    Format.External, Format.Type,
-                    Texture.data(Layer, Face, Level));
-            break;
-        default: assert(0); break;
-        }
-    }
+                /*
+                printf("layer %ld, face %ld, level %ld, iscompressed %d\n",
+                       Layer, Face, Level,
+                       gli::is_compressed(Texture.format()));
+                */
+                if (gli::is_compressed(Texture.format()))
+                    glCompressedTexSubImage2D(
+                                Target, static_cast<GLint>(Level),
+                                0, 0,
+                                Dimensions.x,
+                                Dimensions.y,
+                                Format.Internal, static_cast<GLsizei>(Texture.size(Level)),
+                                Texture.data(Layer, Face, Level));
+                else
+                    glTexSubImage2D(
+                                Target, static_cast<GLint>(Level),
+                                0, 0,
+                                Dimensions.x,
+                                Dimensions.y,
+                                Format.External, Format.Type,
+                                Texture.data(Layer, Face, Level));
+            }
     return TextureName;
 }
 
